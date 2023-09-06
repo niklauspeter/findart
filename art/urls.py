07e1eb9ca9522f5ea0a06f35d8971462f028558d
klaus <oriokiklaus@gmail.com>
2023-09-07 02:11:16 +0300

    add templates, and view models to inosts b madey artists

--- art/urls.py
+++ art/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns=[
-    path('',views.home,name = 'art-home'),
+    # path('',views.home,name = 'art-home'),
+    path('',PostListView.as_view(),name = 'art-home'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name = 'post-detail'),
     path('about/',views.about,name = 'art-about'),
     path('landing/',views.landing,name = 'art-landing'),
 ]
--- art/views.py
+++ art/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 def home(request):
     
     return render(request,'art/index.html')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'art/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def landing(request):
     
