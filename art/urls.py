634de3b43fd9b11990a475678fd653f662cc60bb
klaus <oriokiklaus@gmail.com>
2023-09-07 21:17:36 +0300

    adjust href links for redirection

--- art/urls.py
+++ art/urls.py
@@ -8,8 +8,8 @@ PostDeleteView
 )
 
 urlpatterns=[
-    # path('',views.home,name = 'art-home'),
-    path('',PostListView.as_view(),name = 'art-home'),
+    path('',views.home,name = 'art-home'),
+    path('home/',PostListView.as_view(),name = 'post-home'),
     path('post/<int:pk>/',PostDetailView.as_view(),name = 'post-detail'),
     path('post/new/',PostCreateView.as_view(),name = 'post-create'),
     path('post/<int:pk>/update',PostUpdateView.as_view(),name = 'post-update'),
