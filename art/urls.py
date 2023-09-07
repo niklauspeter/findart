2ecbf4fae7bebd52b183754343c30b739980cf0e
klaus <oriokiklaus@gmail.com>
2023-09-07 03:39:00 +0300

    make it so that only users who are logged in can make uploads

--- art/urls.py
+++ art/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import (PostListView, 
+PostDetailView, 
+PostCreateView,
+PostUpdateView,
+PostDeleteView
+)
 
 urlpatterns=[
     # path('',views.home,name = 'art-home'),
     path('',PostListView.as_view(),name = 'art-home'),
     path('post/<int:pk>/',PostDetailView.as_view(),name = 'post-detail'),
+    path('post/new/',PostCreateView.as_view(),name = 'post-create'),
+    path('post/<int:pk>/update',PostUpdateView.as_view(),name = 'post-update'),
+    path('post/<int:pk>/delete',PostDeleteView.as_view(),name = 'post-delete'),
     path('about/',views.about,name = 'art-about'),
     path('landing/',views.landing,name = 'art-landing'),
 ]
--- art/views.py
+++ art/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
 # Create your views here.
 def home(request):
@@ -10,12 +17,45 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'art/home.html'
+    template_name = 'art/corey_home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.Artist = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.Artist:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.Artist:
+            return True
+        return False
+    
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.Artist = self.request.user
+        return super().form_valid(form)
 
 def landing(request):
     
